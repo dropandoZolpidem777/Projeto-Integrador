6f11c8319022d1c37eaa0d97a377e06ec5089563
Golembr27 <142447072+Golembr27@users.noreply.github.com>
2024-06-03 17:30:14 -0300

    movimentoDoBoss Ok

--- Assets/Scripts/Boss/BossLadoALado.cs
+++ Assets/Scripts/Boss/BossLadoALado.cs
@@ -8,6 +8,8 @@ public class BossLadoALado : MonoBehaviour
     public Transform direita;
     public Transform esquerda;
 
+    public bool indoParaDireita = false;
+
     public float tempoDeMudarDeLado;
 
     public int direcao = 0; // 1- Esquerda 2- Direita
@@ -20,11 +22,31 @@ public class BossLadoALado : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        tempoDeMudarDeLado -= Time.deltaTime;
-        if(tempoDeMudarDeLado <= 0)
+
+        Vector3 movimento = Vector3.zero;
+
+
+        if ( indoParaDireita == true)
         {
-            direcao = UnityEngine.Random.Range(1,2);
+            movimento = Vector3.MoveTowards(transform.position, direita.position, velocidade * Time.deltaTime);
+            if( Vector3.Distance(transform.position, direita.position) < 0.1f)
+            {
+                indoParaDireita = false;
+            }
         }
+        else
+        {
+            movimento = Vector3.MoveTowards(transform.position, esquerda.position, velocidade * Time.deltaTime);
+            Debug.Log(Vector3.Distance(transform.position, esquerda.position));
+            if (Vector3.Distance(transform.position, esquerda.position) < 0.1f)
+            {
+                indoParaDireita = true;
+            }
+        }
+
+        transform.position = movimento;
+
+        return;
         Direita();
         if (direcao == 1)//Esquerda
         {
@@ -40,6 +62,7 @@ public class BossLadoALado : MonoBehaviour
 
     void Direita()
     {
+        
         Vector3 movimento = Vector3.MoveTowards(transform.position, direita.position, velocidade * Time.deltaTime);
         transform.position = movimento;
     }
