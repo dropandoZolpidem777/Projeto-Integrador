29c0692cbb6e1b0e2531443acd78d3d8e26ca0e7
Golembr27 <142447072+Golembr27@users.noreply.github.com>
2024-06-17 17:34:55 -0300

    Arrumando as coisas

--- Assets/Scripts/TrocarCena.cs
+++ Assets/Scripts/TrocarCena.cs
@@ -5,11 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class TrocarCena : MonoBehaviour
 {
+
+    public GameObject ObjAparecer;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            ObjAparecer.SetActive(true);
             SceneManager.LoadScene("Towefight");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ObjAparecer.SetActive(false);
+
         }
     }
 }
